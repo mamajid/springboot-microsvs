9d3a6a31607f82c306d54109ff4c7315aefed9da
Abdul Majid <mamajid@hotmail.com>
2018-04-01 17:32:47 -0400

    First Commit

---
+++ src/main/java/com/amajid/learning/sboot/springbootmicrosvs/SpringbootMicrosvsApplication.java
@@ -0,0 +1,13 @@
+package com.amajid.learning.sboot.springbootmicrosvs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMicrosvsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		SpringApplication.run(SpringbootMicrosvsApplication.class, args);
+	}
+}
