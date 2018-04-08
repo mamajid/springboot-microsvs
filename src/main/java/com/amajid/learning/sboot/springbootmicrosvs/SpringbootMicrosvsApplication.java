93f2075c13d4d9543b68fd533c603b5ed9620374
Abdul Majid <mamajid@hotmail.com>
2018-04-08 17:16:55 -0400

    commit test

--- src/main/java/com/amajid/learning/sboot/springbootmicrosvs/SpringbootMicrosvsApplication.java
+++ src/main/java/com/amajid/learning/sboot/springbootmicrosvs/SpringbootMicrosvsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootMicrosvsApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World\n");
+
 		System.out.println("this should update the github repo and fire-up Jenkins...");
 		System.out.println("Otherwise keep trying....");
 		SpringApplication.run(SpringbootMicrosvsApplication.class, args);
